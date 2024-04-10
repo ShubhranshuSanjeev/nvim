283040f0a34429314360827a3346098321d94c17
ShubhranshuSanjeev <shubhranshu.lumia@gmail.com>
2024-04-10 16:35:36 +0530

    telescope version update

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,7 +1,11 @@
 -- local colorscheme = "rose-pine"
 -- local colorscheme = "gruvbox"
--- local colorscheme = "tokyonight"
-local colorscheme = "kanagawa-dragon"
+-- local colorscheme = "tokyonight-storm"
+-- local colorscheme = "tokyonight-day"
+-- local colorscheme = "tokyonight-moon"
+-- local colorscheme = "tokyonight-night"
+-- local colorscheme = "kanagawa-dragon"
+local colorscheme = "kanagawa-wave"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -71,7 +71,7 @@ return packer.startup(function(use)
   use { "RRethy/vim-illuminate", commit = "d6ca7f77eeaf61b3e6ce9f0e5a978d606df44298" }
 
 	-- Telescope
-	use { "nvim-telescope/telescope.nvim", commit = "76ea9a898d3307244dce3573392dcf2cc38f340f" }
+	use { "nvim-telescope/telescope.nvim", tag = "0.1.4" }
 
 	-- Treesitter
 	use {
@@ -81,7 +81,7 @@ return packer.startup(function(use)
 
 	-- Git
 	use { "lewis6991/gitsigns.nvim", commit = "2c6f96dda47e55fa07052ce2e2141e8367cbaaf2" }
-
+  -- use { 'mrcjkb/rustaceanvim', tag = '3.16.0', ft = { 'rust' } }
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
